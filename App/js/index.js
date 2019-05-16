5844502c31526a7ff69d649c1fc5d260808ef715
Jack Lee <linb.net@gmail.com>
2019-05-16 15:06:35 -0400

    Updated by CrossUI RAD

--- App/js/index.js
+++ App/js/index.js
@@ -341,7 +341,11 @@ xui.Class('App', 'xui.Module',{
                     }
                 })
             );
- 
+            
+            append(
+                xui.create("Module.GitHubAPI", "xui.Module")
+                .setHost(host,"module_githubapi1")
+            );
             
             append(
                 xui.create("xui.UI.CSSBox")
@@ -369,11 +373,7 @@ xui.Class('App', 'xui.Module',{
                     "background-image":"none"
                 })
             );
-                       
-            append(
-                xui.create("Module.GitHubAPI", "xui.Module")
-                .setHost(host,"module_githubapi1")
-            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
