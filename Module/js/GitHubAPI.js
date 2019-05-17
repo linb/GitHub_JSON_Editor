ec9ac70e5cb6cb8904f9043c89ceb0b7be106766
Jack Lee <linb.net@gmail.com>
2019-05-17 00:45:33 -0400

    Updated by CrossUI RAD

--- Module/js/GitHubAPI.js
+++ Module/js/GitHubAPI.js
@@ -18,7 +18,7 @@ xui.Class('Module.GitHubAPI', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
@@ -27,6 +27,7 @@ xui.Class('Module.GitHubAPI', 'xui.Module',{
         customAppend : function(parent, subId, left, top){
             // "return false" will cause all the internal UI controls will be added to the parent panel
             return false;
-        }
+        },
+        functions:{ }
     }
 });
