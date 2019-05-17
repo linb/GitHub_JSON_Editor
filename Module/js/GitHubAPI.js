dcfa093a35debc0c25c4cb5ed52b59afc2246459
Jack Lee <linb.net@gmail.com>
2019-05-17 16:08:45 -0400

    Updated by CrossUI Web App Builder

--- Module/js/GitHubAPI.js
+++ Module/js/GitHubAPI.js
@@ -7,22 +7,7 @@ xui.Class('Module.GitHubAPI', 'xui.Module',{
         properties : {
 //            OctkitCDN:"https://linb.github.io/CrossUI_Assets/thirdparty/octokit-rest.min.js"
         },
-        iniComponents:function(){
-            // [[Code created by CrossUI RAD Studio
-            var host=this, children=[], append=function(child){children.push(child.get(0));};
-
-            append(
-                xui.create("xui.UI.Label")
-                .setHost(host,"xui_ui_label13")
-                .setLeft("10.833333333333334em")
-                .setTop("8.333333333333334em")
-                .setCaption("标 签")
-            );
-
-            return children;
-            // ]]Code created by CrossUI RAD Studio
-        }
-        
+ 
         /*,
         iniResource:function(module, tid){
             xui.Thread.suspend(tid);
