36709480e795e9f4020bbcbb16636566e5b8d497
Jack Lee <linb.net@gmail.com>
2019-05-17 16:09:09 -0400

    Updated by CrossUI Web App Builder

--- Module/js/GitHubAPI.js
+++ Module/js/GitHubAPI.js
@@ -5,19 +5,6 @@ xui.Class('Module.GitHubAPI', 'xui.Module',{
         Required:["https://linb.github.io/CrossUI_Assets/thirdparty/octokit-rest.min.js"],
         // To initialize properties
         properties : {
-//            OctkitCDN:"https://linb.github.io/CrossUI_Assets/thirdparty/octokit-rest.min.js"
-        },
- 
-        /*,
-        iniResource:function(module, tid){
-            xui.Thread.suspend(tid);
-            xui.include("Octokit",module.properties.OctkitCDN,function(){
-                xui.Thread.resume(tid);
-            },function(e){
-                xui.Thread.resume(tid);
-                xui.alert(e);
-                console.log(e);
-            });
-        }*/
+        }
     }
 });
