55a06da7557120289efdca7401c0e663ad76e8ba
Jack Lee <linb.net@gmail.com>
2019-05-17 13:18:52 -0400

    Updated by CrossUI Web App Builder

--- Module/js/GitHubAPI.js
+++ Module/js/GitHubAPI.js
@@ -3,26 +3,9 @@ xui.Class('Module.GitHubAPI', 'xui.Module',{
     Instance:{
         // Dependency classes
         Dependencies:["https://linb.github.io/CrossUI_Assets/thirdparty/octokit-rest.min.js"],
-        // Required modules
-        Required:[],
-
         // To initialize properties
         properties : {
 //            OctkitCDN:"https://linb.github.io/CrossUI_Assets/thirdparty/octokit-rest.min.js"
-        },
-
-        // To initialize instance(e.g. properties)
-        initialize : function(){
-        },
-
-        // To initialize internal components (mostly UI controls)
-        // *** If you're not a skilled, dont modify this function manually ***
-        iniComponents : function(){
-            // [[Code created by CrossUI RAD Studio
-            var host=this, children=[], append=function(child){children.push(child.get(0));};
-            
-            return children;
-            // ]]Code created by CrossUI RAD Studio
         }
         /*,
         iniResource:function(module, tid){
