64463f222a397d37e248d5a8c2205e4fb16dce8d
Jack Lee <linb.net@gmail.com>
2019-05-18 15:52:16 -0400

    Updated by CrossUI Web App Builder

--- Module/js/GitHubAPI.js
+++ Module/js/GitHubAPI.js
@@ -1,6 +1,6 @@
 // It's a module no UI
 xui.Class('Module.GitHubAPI', 'xui.Module',{
-    Instance:{ 
+    Insta nce:{ 
         // Dependency classes
         Required:["https://linb.github.io/CrossUI_Assets/thirdparty/octokit-rest.min.js"],
         // To initialize properties
