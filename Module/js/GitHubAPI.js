6ac6d75c0bddab213d8f2cf580feab9aa4b63cd4
Jack Lee <linb.net@gmail.com>
2019-05-17 22:38:56 -0400

    Updated by CrossUI Web App Builder

--- Module/js/GitHubAPI.js
+++ Module/js/GitHubAPI.js
@@ -11,14 +11,61 @@ xui.Class('Module.GitHubAPI', 'xui.Module',{
             window.xui_GithubHandler = this;
         },        
         ensureGithubAuth:function(){
-            console.log(9);
+            var api = this;
+            var loginLayer = api.loginLayer;
+            if(!loginLayer){
+                loginLayer = api.loginLayer= (new xui.UI.Div())
+                    .setDock('cover')
+                    .setShowEffects("Slide In TB")
+                    .setHideEffects("Slide In TB")
+                    .setPanelBgClr("#fff")
+                    .setIframeAutoLoad("pages/GithubLoginLayer.html");
+                // ensure effects
+                loginLayer.render(true);
+            }
+
+            function showGithubLogInLayer(){
+                var body=xui('body'),
+                    top = body.first().topZindex();
+                loginLayer.setZIndex(top+1).setDisplay('');
+                loginLayer.show(body).setDock('cover',true);
+            }
+
+            var token = api.getToken();
+            if(!token){
+                showGithubLogInLayer();
+            }else{
+                api.clientWithAuth = new Octokit({
+                    auth: 'token ' + token
+                });
+                api.getInstance().users.getAuthenticated().then(function(data){
+                    api.try2getUserCallback(data.data);
+                }, function(err){
+                    api.clientWithAuth = null;
+                    if(err.message == "Bad credentials"){
+                        showGithubLogInLayer();
+                    }
+                });
+            }
+        },
+        getToken:function (){
+            return this._githubAccessToken || ( this._githubAccessToken  = xui.Cookies.get('access_token'));
+        },
+        setToken: function (token){
+            var ns=this;
+            xui.Cookies.set("access_token", ns._githubAccessToken = token);
+        },
+        try2getUserCallback:function(data){
+            var ns = this;
+            ns._userProfile = data;
+            ns.fireEvent("onGithubUserInfo", [data.html_url, data.avatar_url, data]);
         },
         githubTokenResponse:function(tokenHash){
             var ns=this,
                 paras = xui.urlDecode(tokenHash);
             // save to cookie
             if(paras.access_token){
-              ns.setToken(paras.access_token);
+                ns.setToken(paras.access_token);
             }
             ns.ensureGithubAuth();
         }
@@ -26,6 +73,9 @@ xui.Class('Module.GitHubAPI', 'xui.Module',{
     Static:{
         $Functions:{
             ensureGithubAuth:function(){}
+        },
+        $EventHandlers:{
+            onGithubUserInfo: function(html_url, avatar, user){}
         }
     }
 });
