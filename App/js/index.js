5e904d9eaed60cd8229e261e13788d95a0e1f3d6
Jack Lee <linb.net@gmail.com>
2019-05-19 02:28:13 -0400

    Updated by CrossUI Web App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -71,7 +71,7 @@ xui.Class('App', 'xui.Module',{
             );
             
             host.xui_panel_left.append(
-                xui.create("xui.UI.TreeBar")
+                xui.create("xui.UI.TreeView")
                 .setHost(host,"xui_tv_folders")
                 .setDirtyMark(false)
                 .setLeft("0em")
