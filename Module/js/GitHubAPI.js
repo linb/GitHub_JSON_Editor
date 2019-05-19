a3f768d950c9cc58af760aa16f61964af0ffeef0
Jack Lee <linb.net@gmail.com>
2019-05-19 01:46:44 -0400

    Updated by CrossUI Web App Builder

--- Module/js/GitHubAPI.js
+++ Module/js/GitHubAPI.js
@@ -101,7 +101,7 @@ xui.Class('Module.GitHubAPI', 'xui.Module',{
                 var repos = [];
                 rst.data.items.forEach( function(v, i){
                     repos.push({
-                        id:v.path,
+                        id:v.name,
                         caption:v.name,
                         tagVar:v
                     });
