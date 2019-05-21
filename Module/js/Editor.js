0ba6481b2258724fba5651dcaea0e866215dabfe
Jack Lee <linb.net@gmail.com>
2019-05-21 01:12:35 -0400

    Updated by CrossUI Web App Builder

--- Module/js/Editor.js
+++ Module/js/Editor.js
@@ -17,7 +17,28 @@ xui.Class('Module.Editor', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input1")
+                .setDirtyMark(false)
+                .setDock("fill")
+                .setLeft("5.833333333333333em")
+                .setTop("2.5em")
+                .setWidth("18em")
+                .setHeight("10em")
+                .setMultiLines(true)
+            );
+            
+            append(
+                xui.create("xui.UI.Block")
+                .setHost(host,"xui_ui_block19")
+                .setDock("bottom")
+                .setLeft("14.166666666666666em")
+                .setTop("25em")
+                .setHeight("3.3333333333333335em")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
