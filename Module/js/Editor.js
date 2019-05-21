26633eb4fe55b809edbd4ec6259bae5cbf48a53b
Jack Lee <linb.net@gmail.com>
2019-05-21 01:09:43 -0400

    Created by CrossUI Web App Builder

---
+++ Module/js/Editor.js
@@ -0,0 +1,37 @@
+// The default code is a module class (inherited from xui.Module)
+// Ensure that all the value of "key/value pair" does not refer to external variables
+// Sub module don't support Dependencies, and those async functions
+xui.Class('Module.Editor', 'xui.Module',{
+    Instance:{
+        // Required modules
+        Required:[],
+        // To initialize properties
+        properties : {},
+
+        // To initialize instance(e.g. properties)
+        initialize : function(){
+        },
+
+        // To initialize internal components (mostly UI controls)
+        // *** If you're not a skilled, dont modify this function manually ***
+        iniComponents : function(){
+            // [[Code created by CrossUI RAD Studio
+            var host=this, children=[], append=function(child){children.push(child.get(0));};
+
+            return children;
+            // ]]Code created by CrossUI RAD Studio
+        },
+        // To determine how properties affects this module
+        propSetAction : function(prop){
+        }
+    },
+    // export
+    Static:{
+        $DataModel:{
+        },
+        $EventHandlers:{
+        },
+        $Functions:{
+        }
+    }
+});
